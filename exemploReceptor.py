7810761669068ce1a4f5ee2b50bdf5dd3bf20666
André Luiz <42250150+andreluiz2431@users.noreply.github.com>
2021-08-23 19:38:36 +0000

    ininciando E3

--- exemploReceptor.py
+++ exemploReceptor.py
@@ -4,7 +4,6 @@ import os
 import hashlib
 
 server = socket(AF_INET, SOCK_DGRAM)
-
 parser = argparse.ArgumentParser(description='Argumentos')
 parser.add_argument('-p', '--port',type=int, help = 'porta')
 parser.add_argument('-i','--host',type = str, help= 'Ip')
@@ -19,7 +18,6 @@ file_open = open(options.nome_arq,'a') # Abre o arquivo para escrita, passado pe
 
 h = hashlib.md5()
 
-#num = 3567  #numero de sequencia inicial
 while True : # loop infinito
 
     message ,  clientAddress = server.recvfrom(1024) # Recebe a mensagem do transmissor
@@ -30,25 +28,25 @@ while True : # loop infinito
         break
 
     else :
-       
-        envio = message.decode('utf-8').split(' ',2)
+          
+        envio = message.decode('utf-8').split(' ',2) # Divide a string nos dois primeiros espaços
         num_seq = envio[0] #Recebe o numero de sequencia provinda do transmissor
         cod_veri = envio[1]#Recebe o codigo de verificação provinda do transmissor
-        cargaUtil = envio[2]#Recebe a carga util provinda do transmissor
+        cargaUtil = envio[2]#Recebe a carga util provinda do transmissor    
 
-        h.update(str(num_seq).encode())
-        numseq = h.hexdigest()
-
-
-        if((numseq == cod_veri) ):#Verifica se o numero de sequencia e o codigo de verifcação condizem com o esperado
-            server.sendto(str.encode('ACK'),clientAddress)#envia para o transmissor que tudo foi verificado e está OK
-            server.sendto(str.encode('Frame gravado'),clientAddress) # Envia mensagem de confirmação de gravaçao do frame          
-            file_open.write(cargaUtil) # Escreve a mensagem contida no frame no arquivo de saída
-
-        else:   
-            server.sendto(str.encode('NACK'),clientAddress) #Envia para o transmissor que houve um NACK
-            server.sendto(str.encode('Erro na gravaçao'),clientAddress)    
+        h.update(num_seq.encode())
+        numseq = h.hexdigest() # Transforma em hash md5
         
-    
+        if(numseq == cod_veri):#Verifica se o numero de sequencia e o codigo de verifcação condizem com o esperado
+           
+            file_open.write(cargaUtil) # Escreve a mensagem contida no frame no arquivo de saída
+            server.sendto(str.encode('ACK'),clientAddress)#envia para o transmissor que tudo foi verificado e está OK          
+            
+        else: 
+           
+            #error = num_seq +' '+ 'NACK'  
+            server.sendto(str.encode('NACK'),clientAddress) #Envia para o transmissor que houve um NACK
+            #server.sendto(str.encode('Erro na gravaçao no frame'),clientAddress)    
+        
 
 server.close()
--- exemploTransmissor.py
+++ exemploTransmissor.py
@@ -3,6 +3,7 @@ import argparse
 import os
 import hashlib
 import time
+
 i=0
 seq_int =1
 client = socket(AF_INET,SOCK_DGRAM)
@@ -17,41 +18,44 @@ host_andress = (options.host,options.port) # Armazena o host e a porta em uma va
 
 frames = options.frame 
 name_file = options.nome_arq
-file_open = open(options.nome_arq, 'r') # Abre o arquivo para leitura, passado pelo usuario
+file_open = open(options.nome_arq, 'rb') # Abre o arquivo para leitura, passado pelo usuario
 
 file_size = os.path.getsize(options.nome_arq) # Pega o tamanho do arquivo
 div =  (file_size // frames) # Divide o tamanho do arquivo pelos frames
+ 
 
 h = hashlib.md5()
-temporizador =0
-
+client.settimeout(1)    #
 for i in range(div+1) :
-    #time.sleep(5) #temporizador
-    
+ 
     h.update(str(seq_int).encode())
-    cod = h.hexdigest()
+    cod = h.hexdigest() # Transforma o codiog em hash
+ 
+    cargaUtil = file_open.read(frames) # Lê o frame do arquivo
+    envio  = str(seq_int) + ' ' + cod + ' ' + "{}".format(cargaUtil.decode('utf-8'))   
 
-    cargaUtil = file_open.read(frames)
-    envio  = str(seq_int) + ' ' + cod + ' ' + "{}".format(cargaUtil)
-    client.sendto(envio.encode(),(host_andress)) #Envia conteudo contido no tamanho do frame
-
-    request, clientAddress = client.recvfrom(1024)
-    message , clientAddress = client.recvfrom(1024) #Recebe confirmação de envio
+    if(client.sendto(envio.encode(),(host_andress))) : # Envia a o frame jundo com o numero sequecial e o codigo para o receptor
+          
+        try :
     
-    if(request == b'ACK') :
-        print(message.decode('utf-8')) #Printa a confirmação
-        print(request.decode('utf-8'))
-    
-    elif(request == b'NACK') :
-        print(request.decode('utf-8'))
-        print("Recording error")
-    
-    elif(temporizador >1):
-       print("temporizador execedeu ")
+            request, clientAddress = client.recvfrom(1024)#Recebe confirmação de envio
+           
+            if(request == b'ACK') :
+                print(request.decode('utf-8')) # Printa ACK
+                print("Frame Gravado")
+                            
+            elif(request == b'NACK') :
+                print(request.decode('utf-8')) # Printa NACK
+                print("Recording error | Frame  < "+str(seq_int)+" >") # Printa em qual frame deu erro
+            
+        except:
+            print("ERRO Timeout - CLOSE") # Printa se excedeu o tempo e fecha e para de enviar os frames
+            break
+           
     
     seq_int +=1 #Incrementa o numero da sequencia
+   
     
-
 close_file = 'End'
 client.sendto(close_file.encode(),(host_andress)) # Envia uma mensagem de fim
 file_open.close() # Fecha o arquivo
